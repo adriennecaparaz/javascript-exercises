77effd5e418183701cad75755a96947caa843988
Adrienne Caparaz <adcapz@gmail.com>
2022-09-28 00:36:20 -0700

    completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, amount) {
+    let repeatedString = "";
+    if (amount < 0) return 'ERROR';
+    for (i = 0; i < amount; i++) {
+        repeatedString += word;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
