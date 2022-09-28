6e1fbcc5d91656d63be03028491feca30fb7a110
Adrienne Caparaz <adcapz@gmail.com>
2022-09-28 01:29:58 -0700

    completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a != 'number' || typeof b != 'number' || a < 0) {
+        return 'ERROR';
+    }
 
+    let min, max;
+    let sum = 0;
+
+    if (a < b) {
+        min = a;
+        max = b;
+    }
+    else {
+        min = b;
+        max = a;
+    }
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
