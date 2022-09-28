d9002c508127a8846a5235c7d265e2dc97179bc6
Adrienne Caparaz <adcapz@gmail.com>
2022-09-28 01:20:53 -0700

    completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(oldArray, ...toRemove) {
+    const newArray = oldArray;
 
+    for (let i = 0; i < toRemove.length; i++) {
+        for (let j = 0; j < newArray.length; j++) {
+            if (toRemove[i] === newArray[j]) {
+                newArray.splice(j, 1);
+                break;
+            }
+        }
+    }
+    
+    return newArray;
 };
 
 // Do not edit below this line
