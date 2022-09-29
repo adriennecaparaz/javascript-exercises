bf5ed5019f5f0742274c790fff8e0b8f4c401e11
Adrienne Caparaz <adcapz@gmail.com>
2022-09-29 01:41:10 -0700

    completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let phrase = text.replace(/[^A-Za-z]/g, '');
+    let palindrome = phrase.split('');
+    return palindrome.reverse().join('').toLowerCase() === phrase.toLowerCase();
 };
 
 // Do not edit below this line
