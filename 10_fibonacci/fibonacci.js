7ee01ec4e45bf96b085aa237fce23c4620190358
Adrienne Caparaz <adcapz@gmail.com>
2022-09-29 01:54:20 -0700

    completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x < 0) return 'OOPS';
+    let first = 0;
+    let second = 1;
+    for (let i = 1; i <= x; i++) {
+        if (x == 1) return second;
+        if (i == 1) continue; // skip first loop
+        let current = first + second;
+        if (x == i) return current;
+        first = second;
+        second = current;
+    }
 };
 
 // Do not edit below this line
