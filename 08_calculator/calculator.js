636814e0b39c50015704a234929f4accfd2b7dcd
Adrienne Caparaz <adcapz@gmail.com>
2022-09-29 01:22:52 -0700

    completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let sum = 0;
+  for (let num in nums) {
+    sum += nums[num];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let total = 1;
+  for (let num in nums) {
+    total *= nums[num];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1;
+  for (let i = num; i > 0; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
